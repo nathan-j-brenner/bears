cc2a2322d54dde564e993d8876a8d89deb0a868a
Nathan Brenner <nbrenner@cmdagency.com>
2016-05-24 10:45:49 -0700

    route middleware

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 8080;
 mongoose.connect('mongodb://admin:admin@ds011903.mlab.com:11903/dabears');
 var router = express.Router();
+router.use(function (req, res, next) {
+    console.log('Something is happening.');
+    next();
+});
 router.get('/', function (req, res) {
     res.json({ message: 'hooray! welcome to our api!' });
 });
--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import * as express from 'express';
 const app = express();
 import * as bodyParser from 'body-parser';
 import * as mongoose from 'mongoose';
+import * as Bear from './app/models/bears';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -13,6 +14,11 @@ const port = process.env.PORT || 8080;
 mongoose.connect('mongodb://admin:admin@ds011903.mlab.com:11903/dabears')
 
 const router = express.Router();
+
+router.use((req, res, next)=>{
+    console.log('Something is happening.');
+    next();
+});
 
 router.get('/', (req, res)=>{
     res.json({ message: 'hooray! welcome to our api!' });
